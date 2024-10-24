6562bec0bba2e956accd42e61cb48134cbb3c7b1
SidraKayenat <sidrakayenatofficial@gmail.com>
2024-10-23 17:58:25 -0700

    Initial Changes

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,91 +1,88 @@
-import React from 'react';
-import Image from 'next/image';
-import Link from 'next/link';
-import starIcon from '../assets/icons/starIcon.png';
-import rightArrowIcon from '../assets/icons/rightArrowIcon.png';
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import starIcon from "../assets/icons/starIcon.png";
+import rightArrowIcon from "../assets/icons/rightArrowIcon.png";
+import { FaLinkedin, FaFacebook, FaInstagram } from "react-icons/fa"; // Import social media icons
 
 const Footer = () => {
   return (
     <div className="relative p-4 mb-4 mx-[10px] border-t-2 border-solid border-[#EDEFF2]">
-      {/* Row 1 */}
-      <div className="sm:flex justify-center items-center py-[20px] sm:py-[90px] px-2 ">
-        {/* text  */}
-        <p className="text-2xl text-center leading-[30px] font-bold text-[#4D5E80] capitalize mb-10 sm:mb-0 dark:text-white">
-          constructor
-        </p>
-        {/* menu  */}
-        <div className="relative flex gap-5 justify-center items-centers  sm:w-[450px] ">
-          <ul className="grid  sm:grid-cols-3 gap-5 text-center sm:gap-10 ">
-            <li>
-              <Link href="/" className="py-2 hover:text-orange-500">
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link href="/" className="py-2 hover:text-orange-500">
-                Gallery
-              </Link>
-            </li>
-            <li>
-              <Link href="/" className="py-2 hover:text-orange-500">
-                Services
-              </Link>
-            </li>
-            <li>
-              <Link href="/" className="py-2 hover:text-orange-500">
-                About
-              </Link>
-            </li>
-            <li>
-              <Link href="/" className="py-2 hover:text-orange-500">
-                Deals
-              </Link>
-            </li>
-            <li>
-              <Link href="/" className="py-2 hover:text-orange-500">
-                Contacts
-              </Link>
-            </li>
-          </ul>
-        </div>
-        {/* rating */}
-        <div className="flex flex-col items-center sm:items-start gap-1 mt-5 sm:mt-0">
-          <div className="flex">
-            <Image
-              src={starIcon}
-              alt="ratings "
-              className="w-[30px] h-[30px]"
-            />
-            <Image
-              src={starIcon}
-              alt="ratings "
-              className="w-[30px] h-[30px]"
-            />
-            <Image
-              src={starIcon}
-              alt="ratings "
-              className="w-[30px] h-[30px]"
-            />
-            <Image
-              src={starIcon}
-              alt="ratings "
-              className="w-[30px] h-[30px]"
-            />
-            <Image
-              src={starIcon}
-              alt="ratings "
-              className="w-[30px] h-[30px]"
-            />
-          </div>
-          <div className="flex justify-center items-center p-2">
-            <p>5.2M satisfied customers</p>
-          </div>
+      {/* Row 1: Social Links */}
+      <div className="flex flex-col items-center py-5">
+        <div className="flex gap-5">
+          <Link
+            href="#"
+            className="flex items-center justify-center w-10 h-10 rounded-full border border-gray-300 hover:bg-gray-200"
+          >
+            <FaLinkedin className="text-xl" />
+          </Link>
+          <Link
+            href="#"
+            className="flex items-center justify-center w-10 h-10 rounded-full border border-gray-300 hover:bg-gray-200"
+          >
+            <FaFacebook className="text-xl" />
+          </Link>
+          <Link
+            href="#"
+            className="flex items-center justify-center w-10 h-10 rounded-full border border-gray-300 hover:bg-gray-200"
+          >
+            <FaInstagram className="text-xl" />
+          </Link>
         </div>
       </div>
 
-      {/* Row 2 */}
+      {/* Row 2: Main Navigation */}
+      <div className="flex justify-center py-2">
+        <ul className="flex gap-10">
+          <li>
+            <Link href="/" className="py-2 hover:text-orange-500">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link href="/" className="py-2 hover:text-orange-500">
+              Gallery
+            </Link>
+          </li>
+          <li>
+            <Link href="/" className="py-2 hover:text-orange-500">
+              Services
+            </Link>
+          </li>
+          <li>
+            <Link href="/" className="py-2 hover:text-orange-500">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link href="/" className="py-2 hover:text-orange-500">
+              Deals
+            </Link>
+          </li>
+          <li>
+            <Link href="/" className="py-2 hover:text-orange-500">
+              Contacts
+            </Link>
+          </li>
+        </ul>
+      </div>
 
-      <div className="relative py-0 px=[340px] border-t-2 border-solid border-[#EDEFF2] flex items-center justify-between gap-5">
+      {/* Row 3: Secondary Navigation */}
+      <div className="flex justify-center py-2 mb-4">
+        <div className="flex items-center gap-5 text-xs">
+          <Link href="#" className="hover:text-orange-500">
+            TERMS
+          </Link>
+          <div className="border-l border-gray-300 h-4" />
+          <Link href="#" className="hover:text-orange-500">
+            PRIVACY
+          </Link>
+        </div>
+      </div>
+
+      {/* Copyright Section */}
+      <div className="py-4 px-4 w-full bg-blue-500 text-white flex items-center justify-between">
         <p className="capitalize text-[10px] sm:text-[13px] font-semibold leading-[25px] text-center text-bluePText dark:text-white">
           © 2023 Company. All Rights Reserved.
         </p>
@@ -96,20 +93,6 @@ const Footer = () => {
         >
           Design by Spline One
         </Link>
-        <div className="flex items-start p-4 gap-[57px]">
-          <Link
-            href="#"
-            className="capitalize text-[10px] sm:text-[13px] font-semibold leading-[25px] text-center text-bluePText hover:text-orange-500 dark:text-white"
-          >
-            terms
-          </Link>
-          <Link
-            href="#"
-            className="capitalize text-[10px] sm:text-[13px] font-semibold leading-[25px] text-center text-bluePText  hover:text-orange-500 dark:text-white"
-          >
-            privacy
-          </Link>
-        </div>
       </div>
     </div>
   );
